01521ec83854d84be4d1a988d5ed2bfadbcef770
Uyen <nntuyen1996@gmail.com>
2022-05-12 16:22:22 +0700

    init project

---
+++ src/test/java/hellocucumber/StepDefinitions.java
@@ -0,0 +1,37 @@
+package hellocucumber;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class IsItFriday {
+    static String isItFriday(String inputDay) {
+        if (inputDay.toLowerCase().equals("Friday")) {
+            return "YES";
+        } else {
+            return "Nope";
+        }
+    }
+}
+
+public class StepDefinitions {
+    private static String today;
+    private static String actualAnswer;
+
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        today = "Friday";
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = IsItFriday.isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+}
